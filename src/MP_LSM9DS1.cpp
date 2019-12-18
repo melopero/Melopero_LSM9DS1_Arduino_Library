8cd78b28fec2333b3b23166d4ea724996b93ee01
leoli51 <46094699+leoli51@users.noreply.github.com>
2019-12-18 16:02:40 +0100

    user friendly values for input
    some documentation , first draft for error codes

--- src/MP_LSM9DS1.cpp
+++ src/MP_LSM9DS1.cpp
@@ -19,7 +19,7 @@ MP_LSM9DS1::MP_LSM9DS1(){
     this->magMeasurements = {0f,0f,0f};
 }
 
-bool MP_LSM9DS1::useI2C(uint8_t gyroAddress = 0x6b, uint8_t magAddress = 0x1e){
+int8_t MP_LSM9DS1::useI2C(uint8_t gyroAddress = 0x6b, uint8_t magAddress = 0x1e){
         if (I2C_AVAILABLE){
             this->i2cEnabled = true;
             this->spiEnabled = false;
@@ -31,14 +31,14 @@ bool MP_LSM9DS1::useI2C(uint8_t gyroAddress = 0x6b, uint8_t magAddress = 0x1e){
             this->gyroIdentifier = this->i2cGyroAddress;
             this->magIdentifier = this->i2cMagAddress;
             //enable i2c in control registers mag control 3 ...
-            return true;
+            return 0;
         }
         else {
-            return false;
+            return -3;
         }
 }
 
-bool MP_LSM9DS1::useSPI(uint8_t gyroChipSelectPin , uint8_t magChipSelectPin, uint32_t maxTransmissionFreq = 10 ** 7){
+int8_t MP_LSM9DS1::useSPI(uint8_t gyroChipSelectPin , uint8_t magChipSelectPin, uint32_t maxTransmissionFreq = 10 ** 7){
     if (SPI_AVAILABLE){
         this->spiEnabled = true;
         this->i2cEnabled = false;
@@ -64,7 +64,7 @@ bool MP_LSM9DS1::useSPI(uint8_t gyroChipSelectPin , uint8_t magChipSelectPin, ui
             this->spiMagPin = gyroChipSelectPin;
         }
         else {
-            return false;
+            return -4;
         }
 
         this->gyroIdentifier = this->spiGyroPin;
@@ -77,10 +77,10 @@ bool MP_LSM9DS1::useSPI(uint8_t gyroChipSelectPin , uint8_t magChipSelectPin, ui
         this->writeFlagMag(MAG_CONTROL_REG_3, 0b1, 3, 1)
         //TODO: disable i2c ?
 
-        return true;
+        return 0;
     }
     else {
-        return false;
+        return -4;
     }
 }
 
@@ -135,17 +135,6 @@ uint8_t MP_LSM9DS1::writeByte(uint8_t deviceIdentifier, uint8_t registerAddress,
     return bytesWritten;
 }
 
-
-
-/**Writes the flag bits in the spcified register at the specified position.\n
-*start : start of the flag bits, the MSB has an index of 8 and the lsb has an index of 1.\n
-*length : how many bits of the flag to set.\n
-*Example:
-*register value = 0b11110000\n
-*flag value = 0b0011\n
-*start = 6\n
-*length = 4\n
-*result = 0b11001100 */
 uint8_t MP_LSM9DS1::writeFlag(uint8_t deviceIdentifier, uint8_t registerAddress, uint8_t flag, uint8_t start, uint8_t length){
     uint8_t end = start - length;
     uint8_t regContent = this->readByte(deviceIdentifier, registerAddress);
@@ -165,27 +154,29 @@ void MP_LSM9DS1::close(){
         //GPIO cleanup!    }
 }
 
-void MP_LSM9DS1::resetSettings(){
-    this->writeFlag(this->gyroIdentifier, CONTROL_REG_8, 0b101, 3, 3);
-    this->writeFlag(this->magIdentifier, MAG_CONTROL_REG_2, 0b11, 4, 2);
-    this->resetInterruptSettings();
+int8_t MP_LSM9DS1::resetSettings(){
+    int8_t bytesWritten = 0;
+    bytesWritten = this->writeFlag(this->gyroIdentifier, CONTROL_REG_8, 0b101, 3, 3);
+    if (bytesWritten == 0) return -1;
+    bytesWritten = this->writeFlag(this->magIdentifier, MAG_CONTROL_REG_2, 0b11, 4, 2);
+    if (bytesWritten == 0) return -2;
+    return this->resetInterruptSettings();
 }
 
-void MP_LSM9DS1::setGyroODR(OutputDataRate odr){
-    /**Sets the accelerometer output-data-rate. */
-    this->writeFlag(this->gyroIdentifier, GYRO_CONTROL_REG_1, odr, 8, 3);
+int8_t MP_LSM9DS1::setGyroODR(OutputDataRate odr){
+    return this->writeFlag(this->gyroIdentifier, GYRO_CONTROL_REG_1, odr, 8, 3) > 0 ? 0 : -1;
 }
 
-void MP_LSM9DS1::setAccODR(OutputDataRate odr){
-    /**Sets the accelerometer output-data-rate. */
-    this->writeFlag(this->gyroIdentifier, ACC_CONTROL_REG_6, odr, 8, 3);
+int8_t MP_LSM9DS1::setAccODR(OutputDataRate odr){
+    return this->writeFlag(this->gyroIdentifier, ACC_CONTROL_REG_6, odr, 8, 3) > 0 ? 0 : -1;
 }
 
-void MP_LSM9DS1::setMagODR(MagnetometerOutputDataRate odr, MagnetometerMode mode = MagnetometerMode::CONTINUOUS){
-    /** Sets the magnetometer output-data-rate and operating mode.
-    (continuous, single conversion, power down). */
-    this->writeFlag(this.magIdentifier, MAG_CONTROL_REG_3, mode, 2, 2);
-    this->writeFlag(this->magIdentifier, MAG_CONTROL_REG_1, odr, 5, 3);
+int8_t MP_LSM9DS1::setMagODR(MagnetometerOutputDataRate odr, MagnetometerMode mode = MagnetometerMode::CONTINUOUS){
+    int8_t bytesWritten = 0;
+    bytesWritten = this->writeFlag(this.magIdentifier, MAG_CONTROL_REG_3, mode, 2, 2);
+    if (bytesWritten == 0) return -2;
+    bytesWritten = this->writeFlag(this->magIdentifier, MAG_CONTROL_REG_1, odr, 5, 3);
+    return bytesWritten > 0 ? 0 : -2;
 }
 
 int8_t MP_LSM9DS1::setGyroRange(GyroscopeRange range){
@@ -197,10 +188,12 @@ int8_t MP_LSM9DS1::setGyroRange(GyroscopeRange range){
     else if (range == GyroscopeRange::AR_2000DPS)
         flag = 0b11;
     else
-        return -1;
+        return -5;
 
     this->gyroScale = range;
-    return this->writeFlag(this->gyroIdentifier, GYRO_CONTROL_REG_1, flag, 5, 2);
+    int8_t bytesWritten = 0;
+    bytesWritten = this->writeFlag(this->gyroIdentifier, GYRO_CONTROL_REG_1, flag, 5, 2);
+    return bytesWritten == 0 ? -1 : 0;
 }
 
 
@@ -215,10 +208,12 @@ int8_t MP_LSM9DS1::setAccRange(AccelerometerRange range){
     else if (range == AccelerometerRange::16_g)
         flag = 0b01;
     else
-        return -1;
+        return -5;
 
     this->accScale = range;
-    this->writeFlag(this->gyroIdentifier, ACC_CONTROL_REG_6, flag, 5, 2);
+    int8_t bytesWritten = 0;
+    bytesWritten = this->writeFlag(this->gyroIdentifier, ACC_CONTROL_REG_6, flag, 5, 2);
+    return bytesWritten == 0 ? -1 : 0;
 }
 
 
@@ -233,33 +228,45 @@ int8_t MP_LSM9DS1::setMagRange(MagnetometerRange range){
     else if (range == MagnetometerRange::16_G)
         flag = 0b11;
     else
-        return -1;
+        return -5;
 
     this->magScale = range;
-    this->writeFlag(this->magIdentifier, MAG_CONTROL_REG_2, flag, 7, 2);
+    int8_t bytesWritten = 0;
+    bytesWritten = this->writeFlag(this->magIdentifier, MAG_CONTROL_REG_2, flag, 7, 2);
+    return bytesWritten == 0 ? -2 : 0;
 }
 
 int8_t MP_LSM9DS1::resetInterruptSettings(bool resetAccInterrupt = true, bool resetGyroInterrupt = true, bool resetMagInterrupt = true){
-    if (resetAccInterrupt)
-        this->writeByte(this->gyroIdentifier, ACC_INT_CFG_REG, 0x00);
-    if (resetGyroInterrupt)
-        this->writeByte(this->gyroIdentifier, GYRO_INT_CFG_REG, 0x00);
-    if (resetMagInterrupt)
-        this->writeByte(this->magIdentifier, MAG_INT_CFG_REG, 0x00);
+    int8_t bytesWritten = 0;
+    if (resetAccInterrupt){
+        bytesWritten = this->writeByte(this->gyroIdentifier, ACC_INT_CFG_REG, 0x00);
+        if (bytesWritten == 0) return -1;
+    }
+    if (resetGyroInterrupt){
+        bytesWritten = this->writeByte(this->gyroIdentifier, GYRO_INT_CFG_REG, 0x00);
+        if (bytesWritten == 0) return -1;
+    }
+    if (resetMagInterrupt){
+        bytesWritten = this->writeByte(this->magIdentifier, MAG_INT_CFG_REG, 0x00);
+        if (bytesWritten == 0) return -2;
+    }
+
+    return 0;
 }
 
-int8_t MP_LSM9DS1::setAccInterrupt( uint8_t xThreshold, bool xDetect, bool xDetectHigh,
-                                    uint8_t yThreshold, bool yDetect, bool yDetectHigh,
-                                    uint8_t zThreshold, bool zDetect, bool zDetectHigh,
+
+int8_t MP_LSM9DS1::setAccInterrupt( float xThreshold, bool xDetect, bool xDetectHigh,
+                                    float yThreshold, bool yDetect, bool yDetectHigh,
+                                    float zThreshold, bool zDetect, bool zDetectHigh,
                                     bool andEventCombination = false, uint8_t samplesToRecognize = 0,
                                     bool waitBeforeExitingInterrupt = false, hardwareInterrupt = false){
 
-    if (samplesToRecognize > 0x7F) return -1;
+    if (samplesToRecognize > 0x7F) return -5;
 
     #and/or event combination + reset other settings
     this->writeByte(this->gyroIdentifier, ACC_INT_CFG_REG, andEventCombination ? 0x80 : 0x00);
 
-    uint8_t accRange = -1;
+    float accRange = -1;
     switch (this->accScale){
         case AccelerometerRange::2_g: accRange = 2; break;
         case AccelerometerRange::4_g: accRange = 4; break;
@@ -267,40 +274,49 @@ int8_t MP_LSM9DS1::setAccInterrupt( uint8_t xThreshold, bool xDetect, bool xDete
         case AccelerometerRange::16_g: accRange = 16; break;
     }
 
+    //real interrupt : acc_range / raw_threshold therefore : float_thr = acc_range / raw_ths -> raw_ths = acc_range / float thr
     if (xDetect){
-        this->writeByte(this->gyroIdentifier, ACC_X_INT_THR_REG, xThreshold);
+        uint8_t rawXThreshold = xThreshold == 0 ? 0 : (uint8_t) (accRange / xThreshold);
+        this->writeByte(this->gyroIdentifier, ACC_X_INT_THR_REG, rawXThreshold);
         this->writeFlag(this->gyroIdentifier, ACC_INT_CFG_REG, xDetectHigh ? 0b10 : 0b01, 2, 2);
     }
     if (yDetect){
-        this->writeByte(this->gyroIdentifier, ACC_Y_INT_THR_REG, yThreshold);
+        uint8_t rawYThreshold = yThreshold == 0 ? 0 : (uint8_t) (accRange / yThreshold);
+        this->writeByte(this->gyroIdentifier, ACC_Y_INT_THR_REG, rawYThreshold);
         this->writeFlag(this->gyroIdentifier, ACC_INT_CFG_REG, yDetectHigh ? 0b10 : 0b01, 4, 2);
     }
     if (zDetect){
-        this->writeByte(this->gyroIdentifier, ACC_Z_INT_THR_REG, zThreshold);
+        uint8_t rawZThreshold = zThreshold == 0 ? 0 : (uint8_t) (accRange / zThreshold);
+        this->writeByte(this->gyroIdentifier, ACC_Z_INT_THR_REG, rawZThreshold);
         this->writeFlag(this->gyroIdentifier, ACC_INT_CFG_REG, zDetectHigh ? 0b10 : 0b01, 6, 2);
     }
 
     this->writeByte(this->gyroIdentifier, ACC_INT_DUR_REG, samplesToRecognize | waitBeforeExitingInterrupt ? 0x80 : 0x00);
     this->writeFlag(this->gyroIdentifier, INT1_CTRL_REG, hardwareInterrupt? 1 : 0, 7, 1);
+
+    return 0;
 }
 
-int8_t setGyroInterrupt(int16_t xThreshold, bool xDetect, bool xDetectHigh,
-                        int16_t yThreshold, bool yDetect, bool yDetectHigh,
-                        int16_t zThreshold, bool zDetect, bool zDetectHigh,
+int8_t setGyroInterrupt(float xThreshold, bool xDetect, bool xDetectHigh,
+                        float yThreshold, bool yDetect, bool yDetectHigh,
+                        float zThreshold, bool zDetect, bool zDetectHigh,
                         bool andEventCombination = false, uint8_t samplesToRecognize = 0,
                         bool waitBeforeExitingInterrupt = false, bool decrementCounter = false,
                         bool hardwareInterrupt = false){
 
     if (samplesToRecognize > 0x7F)
-        return -1;
+        return -5;
 
     #reset interrupt
     this->writeByte(this->gyroIdentifier, GYRO_INT_CFG_REG, andEventCombination ? 0x80 : 0x00);
 
+    //x_threshold = int(x_threshold * 1000 / self.gyro_scale)
+
     if (xDetect){
-        uint16_t 15_bitXThreshold = this->to15BitWord(xThreshold);
+        int16_t rawXThreshold = (int16_t) ( xThreshold * 1000 / this->gyroScale)
+        uint16_t 15_bitXThreshold = this->to15BitWord(rawXThreshold);
         if (15_bitXThreshold == 0x8000)
-            return -1;
+            return -5;
         15_bitXThreshold |= decrementCounter ? 0x8000 : 0x0000;
         this->writeByte(this->gyroIdentifier, GYRO_X_INT_THR_REG, (uint8_t) (15_bitXThreshold / (1 << 8)));
         this->writeByte(this->gyroIdentifier, GYRO_X_INT_THR_REG + 1, (uint8_t) (15_bitXThreshold % (1 << 7)));
@@ -308,24 +324,28 @@ int8_t setGyroInterrupt(int16_t xThreshold, bool xDetect, bool xDetectHigh,
     }
 
     if (yDetect){
-        uint16_t 15_bitYThreshold = this->to15BitWord(yThreshold);
+        int16_t rawYThreshold = (int16_t) ( yThreshold * 1000 / this->gyroScale)
+        uint16_t 15_bitYThreshold = this->to15BitWord(rawYThreshold);
         if (15_bitYThreshold == 0x8000)
-            return -1;
+            return -5;
         this->writeByte(this->gyroIdentifier, GYRO_Y_INT_THR_REG, (uint8_t) (15_bitYThreshold / (1 << 8)));
         this->writeByte(this->gyroIdentifier, GYRO_Y_INT_THR_REG + 1, (uint8_t) (15_bitYThreshold % (1 << 7)));
         this->writeFlag(this->gyroIdentifier, GYRO_INT_CFG_REG, yDetectHigh ? 0b10 : 0b01, 4, 2);
     }
 
     if (zDetect){
-        uint16_t 15_bitZThreshold = this->to15BitWord(zThreshold);
+        int16_t rawZThreshold = (int16_t) ( zThreshold * 1000 / this->gyroScale)
+        uint16_t 15_bitZThreshold = this->to15BitWord(rawZThreshold);
         if (15_bitZThreshold == 0x8000)
-            return -1;
+            return -5;
         this->writeByte(this->gyroIdentifier, GYRO_Z_INT_THR_REG, (uint8_t) (15_bitZThreshold / (1 << 8)));
         this->writeByte(this->gyroIdentifier, GYRO_Z_INT_THR_REG + 1, (uint8_t) (15_bitZThreshold % (1 << 7)));
         this->writeFlag(this->gyroIdentifier, GYRO_INT_CFG_REG, ZDetectHigh ? 0b10 : 0b01, 6, 2);    }
 
     this->writeByte(this->gyroIdentifier, GYRO_INT_DUR_REG, samplesToRecognize | (waitBeforeExitingInterrupt ? 0x80 : 0x00));
     this->writeFlag(this->gyroIdentifier, INT1_CTRL_REG, hardwareInterrupt ? 1 : 0, 8, 1);
+
+    return 0;
 }
 
 uint16_t MP_LSM9DS1::to15BitWord(int16_t value){
@@ -343,9 +363,9 @@ uint16_t MP_LSM9DS1::to15BitWord(int16_t value){
     }}
 
 
-int8_t MP_LSM9DS1::setMagInterrupt( uint16_t threshold, bool xDetect, bool yDetect, bool zDetect,
+int8_t MP_LSM9DS1::setMagInterrupt( float threshold, bool xDetect, bool yDetect, bool zDetect,
                                     bool interruptActiveHigh =false, bool latchInterrupt = false,
-                                    bool hardware_interrupt = false){
+                                    bool hardwareInterrupt = false){
     uint8_t flag = 0x00;
     flag |= xDetect << 7;
     flag |= yDetect << 6;
@@ -357,13 +377,16 @@ int8_t MP_LSM9DS1::setMagInterrupt( uint16_t threshold, bool xDetect, bool yDete
     this->writeByte(this->magIdentifier, MAG_INT_CFG_REG, flag);
 
     if (xDetect || yDetect || zDetect){
-        uint16_t uint15_threshold = (int) (threshold * 1000 / this->magScale);
-        if (uint15_threshold > 0x7FFF or uint15_threshold < 0)
-            return -1; // threshold out of range
-        this->writeByte(this->magIdentifier, MAG_INT_THR_REG, (uint8_t) (uint15_threshold % (1<<7)));
-        this->writeByte(this->magIdentifier, MAG_INT_THR_REG + 1, (uint8_t) (uint15_threshold / (1 << 8)));
-
+        if (threshold < 0)
+            threshold = -threshold; // threshold out of ranfe
+        uint16_t uint15Threshold = (uint16_t) (threshold * 1000 / this->magScale);
+        if (uint15Threshold > 0x7FFF)
+            return -5; // threshold out of range
+        this->writeByte(this->magIdentifier, MAG_INT_THR_REG, (uint8_t) (uint15Threshold % (1<<7)));
+        this->writeByte(this->magIdentifier, MAG_INT_THR_REG + 1, (uint8_t) (uint15Threshold / (1 << 8)));
     }
+
+    return 0;
 }
 
 uint8_t MP_LSM9DS1::getAccInterruptStatus(){
@@ -399,7 +422,7 @@ void MP_LSM9DS1::updateAccMeasurements(){
     }
 }
 
-void MP_LSM9DS1::updateMagMeasurements(){def get_mag(self):
+void MP_LSM9DS1::updateMagMeasurements(){
     uint8_t axisRegs[3] = {MAG_X_REG, MAG_Y_REG, MAG_Z_REG};
     for (uint8_t i = 0; i <= 2; i++){
         this->magRawMeasurements[i] = this->readByte(this->magIdentifier, axisRegs[i]);
@@ -407,4 +430,16 @@ void MP_LSM9DS1::updateMagMeasurements(){def get_mag(self):
 
         this->magMeasurements[i] = ((float) this->magRawMeasurements[i]) / 1000.0f * this->magScale;
     }
+}
+
+char[] MP_LSM9DS1::getErrorString(int8_t errorCode){
+    if (errorCode >= 0) return "No error / Number of bytes written";
+    if (errorCode == -1) return "Communication Error with the gyroscope sensor.";
+    if (errorCode == -2) return "Communication Error with the magnetometer sensor.";
+    if (errorCode == -3) return "I2C communication channel error";
+    if (errorCode == -4) return "SPI communication channel error";
+    if (errorCode == -5) return "Invalid data format/range (input data out of range)";
+    else return "Unknown error";
+}
+
 }
--- src/MP_LSM9DS1.h
+++ src/MP_LSM9DS1.h
@@ -17,8 +17,6 @@
 #pragma message ( "warning : You need SPI.h in order to use the SPI protocol." )
 #endif // __has_include spi
 
-
-
 #define WHO_AM_I_REG 0x0F
 #define GYRO_ID 0b01101000
 #define MAG_ID 0b00111101
@@ -150,39 +148,96 @@ class MP_LSM9DS1 {
 
     //methods
     public:
-        bool useI2C(uint8_t gyroAddress = 0x6b, uint8_t magAddress = 0x1e);
-        bool useSPI(uint8_t gyroChipSelectPin , uint8_t magnetometerChipSelectPin, uint32_t maxTransmissionFreq = 10 ** 7);
+        int8_t useI2C(uint8_t gyroAddress = 0x6b, uint8_t magAddress = 0x1e);
+        int8_t useSPI(uint8_t gyroChipSelectPin , uint8_t magnetometerChipSelectPin, uint32_t maxTransmissionFreq = 10 ** 7);
 
         uint8_t readByte(uint8_t deviceIdentifier, uint8_t registerAddress);
         uint8_t writeByte(uint8_t deviceIdentifier, uint8_t registerAddress, uint8_t value);
 
+        /**Writes the flag bits in the spcified register at the specified position.\n
+         * start : start of the flag bits, the MSB has an index of 8 and the lsb has an index of 1.\n
+         * length : how many bits of the flag to set.\n
+         * Example:
+         * register value = 0b11110000\n
+         * flag value = 0b0011\n
+         * start = 6\n
+         * length = 4\n
+         * result = 0b11001100 */
         uint8_t writeFlag(uint8_t deviceIdentifier, uint8_t registerAddress, uint8_t flag, uint8_t start, uint8_t length);
 
         void close();
-        void resetSettings();
+        int8_t resetSettings();
 
-        void setGyroODR(OutputDataRate odr);
-        void setAccODR(OutputDataRate odr);
-        void setMagODR(MagnetometerOutputDataRate odr, MagnetometerMode mode = MagnetometerMode::CONTINUOUS);
+
+        /** Sets the accelerometer output-data-rate. */
+        int8_t setGyroODR(OutputDataRate odr);
+
+
+        /** Sets the accelerometer output-data-rate. */
+        int8_t setAccODR(OutputDataRate odr);
+
+
+        /** Sets the magnetometer output-data-rate and operating mode.
+         *  (continuous, single conversion, power down). */
+        int8_t setMagODR(MagnetometerOutputDataRate odr, MagnetometerMode mode = MagnetometerMode::CONTINUOUS);
 
         int8_t setGyroRange(GyroscopeRange range);
         int8_t setAccRange(AccelerometerRange range);
         int8_t setMagRange(MagnetometerRange range);
 
-        int8_t setAccInterrupt( uint8_t xThreshold, bool xDetect, bool xDetectHigh,
-                                uint8_t yThreshold, bool yDetect, bool yDetectHigh,
-                                uint8_t zThreshold, bool zDetect, bool zDetectHigh,
+        /**
+         * @param x/y/zThreshold When a measure exceeds (or is lower) than the threshold
+         *        value it triggers an interrupt. This values are expressed in g's (gravity = 9.8 ms^-2)
+         *        The given value will be converted to an unsigned 8 bit int through this formula :
+         *        rawThreshold = (uint8_t) (acceleratorRange / Threshold)
+         * @param x/y/zDetect wether the given axis should be detected.
+         * @param x/y/zDetectHigh if True detects samples that are over the threshold.
+         * @param andEventCombination if True all specified interrupt conditions must be
+         *        triggered to generate an interrupt
+         * @param samplesToRecognize the number of samples that trigger the interrupt to measure
+         *        before actually triggering the interrupt
+         * @param waitBeforeExitingInterrupt the number of samples to measure before exiting the interrupt
+         * @param hardwareInterrupt if True generates an hardware interrupt on the int1 pin
+         */
+        int8_t setAccInterrupt( float xThreshold, bool xDetect, bool xDetectHigh,
+                                float yThreshold, bool yDetect, bool yDetectHigh,
+                                float zThreshold, bool zDetect, bool zDetectHigh,
                                 bool andEventCombination = false, uint8_t samplesToRecognize = 0,
                                 bool waitBeforeExitingInterrupt = false, hardwareInterrupt = false);
 
-        int8_t setGyroInterrupt(int16_t xThreshold, bool xDetect, bool xDetectHigh,
-                                int16_t yThreshold, bool yDetect, bool yDetectHigh,
-                                int16_t zThreshold, bool zDetect, bool zDetectHigh,
+        /**
+         * @param x/y/zThreshold the interrupt threshold value expressed in dps (degrees per second).
+         *        The given values will be converted to a signed 15 bit integer through this formula :
+         *        rawThreshold = to15Bit((int16_t) ( Threshold * 1000 / this->gyroScale))
+         * @param x/y/zDetect wether the given axis should be detected.
+         * @param x/y/zDetectHigh if True detects samples that are over the threshold.
+         * @param andEventCombination if True all specified interrupt conditions must be
+         *        triggered to generate an interrupt
+         * @param samplesToRecognize the number of samples that trigger the interrupt to measure
+         *        before actually triggering the interrupt
+         * @param waitBeforeExitingInterrupt the number of samples to measure before exiting the interrupt
+         * @param decrementCounter
+         * @param hardwareInterrupt if True generates an hardware interrupt on the int1 pin
+         */
+        int8_t setGyroInterrupt(float xThreshold, bool xDetect, bool xDetectHigh,
+                                float yThreshold, bool yDetect, bool yDetectHigh,
+                                float zThreshold, bool zDetect, bool zDetectHigh,
                                 bool andEventCombination = false, uint8_t samplesToRecognize = 0,
                                 bool waitBeforeExitingInterrupt = false, bool decrementCounter = false,
                                 bool hardwareInterrupt = false);
 
-        int8_t setMagInterrupt( uint16_t threshold, bool xDetect, bool yDetect, bool zDetect,
+        /**
+         * @param threshold If measurement exceeds on positively (or negatively) the
+         *        threshold, an interrupt is generated, This value is expressed in gauss and must be positive.
+         *        The given values will be converted to an unsigned 15 bit integer with this formula:
+         *        uint16_t uint15Threshold = (uint16_t) (threshold * 1000 / this->magScale);
+         * @param x/y/zDetect if True detects samples from the specified axes
+         * @param interruptActiveHigh if True the interrupt signal on the INT_M pin is HIGH
+         * @param latchInterrupt Once latched, the INT_M pin remains in the same
+                  state until getMagInterrupt is called.
+         * @param hardwareInterrupt if True generates an hardware interrupt on the INT_M pin
+         */
+        int8_t setMagInterrupt( float threshold, bool xDetect, bool yDetect, bool zDetect,
                                 bool interruptActiveHigh =false, bool latchInterrupt = false,
                                 bool hardware_interrupt = false);
 
@@ -195,6 +250,8 @@ class MP_LSM9DS1 {
         void updateGyroMeasurements();
         void updateAccMeasurements();
         void updateMagMeasurements();
+
+        char[] getErrorString(int8_t errorCode);
 
     private:
         uint16_t to15BitWord(int16_t value);
